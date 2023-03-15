701c0bc6a400295e6ae12494f15599328b28dcbe
Davide Di GIovanni <davidedg97@gmail.com>
2023-03-15 17:37:38 +0100

    dokerfile

--- demo/src/main/java/com/utentibelli/demo/controller/UtenteController.java
+++ demo/src/main/java/com/utentibelli/demo/controller/UtenteController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.utentibelli.demo.model.Utente;
 import com.utentibelli.demo.service.UtenteService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class UtenteController { 
